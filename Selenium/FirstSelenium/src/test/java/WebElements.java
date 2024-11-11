595543eb32cb53d46d6f51731e63fd619947e189
Ventsislav Peychev <del.busque@gmail.com>
2024-11-11 20:12:12 +0200

    Add find by cssSelector

--- Selenium/FirstSelenium/src/test/java/WebElements.java
+++ Selenium/FirstSelenium/src/test/java/WebElements.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.List;
 
 public class WebElements {
@@ -13,9 +12,11 @@ public class WebElements {
 
         driver.get("https://the-internet.herokuapp.com/");
         driver.findElement(By.linkText("Sortable Data Tables")).click();
-        WebElement table = driver.findElement(By.id("table1"));
+        // WebElement table = driver.findElement(By.id("table1"));
+        WebElement table = driver.findElement(By.cssSelector("table#table1"));
 
         List<WebElement> tableRows = table.findElements(By.tagName("tr"));
+        System.out.println(tableRows.get(1).getText());
 
 
         Thread.sleep(3000);
