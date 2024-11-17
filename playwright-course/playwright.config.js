7c2b37f09b9eca78064ee847b4bba383fd424945
Ventsislav Peychev <del.busque@gmail.com>
2024-11-17 16:12:51 +0200

    Create playwright.config.js

---
+++ playwright-course/playwright.config.js
@@ -0,0 +1,25 @@
+const { PlaywrightTestConfig } = require('@playwright/test')
+
+const config = {
+    retries: 1,
+    timeout: 60000,
+    use: {
+        headless: true,
+        viewport: { width: 1280, height: 720 },
+        video: 'off',
+        screenshots: 'off'
+    },
+
+    projects: [
+        {
+            name: 'Chrome',
+            use: { browserName: 'chromium' }
+        },
+        {
+            name: 'Webkit',
+            use: { browserName: 'webkit' }
+        }
+    ]
+}
+
+module.exports = config
