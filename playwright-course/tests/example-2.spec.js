618d6ba85562ba57c89f7a709962d033c825f85e
Ventsislav Peychev <del.busque@gmail.com>
2024-11-17 14:22:40 +0200

    Add test tagging

--- playwright-course/tests/example-2.spec.js
+++ playwright-course/tests/example-2.spec.js
@@ -1,20 +1,19 @@
 const { test, expect } = require('@playwright/test')
 
-test('First Test', async ({ page }) => {
+test("First Test", { tag: ['@smoke'] }, async ({ page }) => {
     await page.goto('https://playwright.dev/docs/intro')
     const title = page.locator('.navbar__title')
     await expect(title).toHaveText('Playwright')
 })
 
-test.only("Simple Click Test", async ({ page, browserName }) => {
-    test.skip(browserName === 'chromium', 'Skipped on Chromium')
+test("Simple Click Test @regressions-smoke", async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/')
     await page.click('text=Add/Remove Elements')
     const element = page.locator('text=Add Element')
     await element.click()
 })
 
-test.skip("Dublicated Test", async ({ page }) => {
+test("Dublicated Test @regression", async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/')
     await page.click('text=Add/Remove Elements')
     const element = page.locator('text=Add Element')
