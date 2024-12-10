22ea75fda354273e927bf2feb8308c241301b6e5
Ventsislav Peychev <del.busque@gmail.com>
2024-12-10 09:31:06 +0200

    Add drop-down

--- playwright-course/tests/example-3.spec.js
+++ playwright-course/tests/example-3.spec.js
@@ -1,8 +1,8 @@
-const { test } = require('@playwright/test')
+const { test, expect } = require('@playwright/test')
 
 test.describe('Examples', () => {
 
-    test.skip('Box check/uncheck', async ({ page }) => {
+    test('Box check/uncheck', async ({ page }) => {
         await page.goto('https://the-internet.herokuapp.com/checkboxes')
         await page.getByRole('checkbox').first().check()
         await page.locator('input[type="checkbox"]').nth(1).uncheck()
@@ -13,6 +13,15 @@ test.describe('Examples', () => {
     test('Drag and drop', async ({ page }) => {
         await page.goto('https://the-internet.herokuapp.com/drag_and_drop')
         await page.dragAndDrop('#column-a', '#column-b')
+        await page.pause()
+    })
+
+    test.only('Drop down', async ({ page }) => {
+        await page.goto('https://the-internet.herokuapp.com/dropdown')
+        await page.locator('select').selectOption('1')
+        await expect(page.locator('select')).toHaveValue('1')
+        await page.locator('#dropdown').selectOption({ label: 'Option 2' })
+        await expect(page.locator('#dropdown')).toHaveValue('2')
         await page.pause()
     })
 })
